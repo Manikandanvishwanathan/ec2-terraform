ba3dafe418c98a35195126c2bdc30a512798376a
Manikandanvishwanathan <131459649+Manikandanvishwanathan@users.noreply.github.com>
2023-08-17 20:05:30 +0530

    Update main.tf
    updated file

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region = "us-east-1"
+    region = "us-east-1"  
+}
+
+resource "aws_instance" "foo" {
+  ami           = "ami-05fa00d4c63e32376" # us-west-2
+  instance_type = "t2.micro"
+  tags = {
+      Name = "TF-Instance"
+  }
 }
